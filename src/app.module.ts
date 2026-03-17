df69c695db32dfd50381ba064a9f71564f9fd6bf
andregarcia0412 <andregarcia0412@gmail.com>
2026-03-17 17:15:40 -0300

    :sparkles: feature: classification module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { ClassificationModule } from './module/classification/classification.module';
+import { SystemModule } from './module/system/system.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    ClassificationModule,
+    SystemModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
