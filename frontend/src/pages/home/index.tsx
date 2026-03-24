00b439aa1701f3cea916f18009f3aa9db4604075
andregarcia0412 <andregarcia0412@gmail.com>
2026-03-24 17:19:22 -0300

    :sparkles: feature: home page styling

--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -1,9 +1,47 @@
-import "./style.css"
+import { Dropzone } from "../../components/Dropzone/Dropzone";
+import "./style.css";
+import VitalSignsGrey from "../../assets/vital_signs_grey.svg";
 
 export const Home = () => {
-    return (
-        <div>
-            <h1>This is home</h1>
+  return (
+    <div className="home-layout">
+      <div className="home-column home-column-left">
+        <Dropzone loading={false} analyzeOnClick={() => {}} />
+
+        <div className="home-card about-card">
+          <h1>Sobre o sistema</h1>
+          <p>
+            O MediScan AI utiliza algoritmos de inteligência artificial para
+            auxiliar na detecção de condições pulmonares através de imagens de
+            raio-X.
+          </p>
+
+          <div className="about-card-list">
+            <ul>
+              <li>Suporta imagens JPEG, PNG e outros formatos comuns</li>
+              <li>
+                Detecta COVID-19, pneumonia bacteriana e condições normais
+              </li>
+              <li>Fornece nível de confiança para cada diagnóstico</li>
+            </ul>
+          </div>
         </div>
-    )
-}
+      </div>
+
+      <div className="home-column home-column-right">
+        <div className="home-card home-card-fill">
+          <div className="home-card-waiting">
+            <div className="vital-signs-grey-icon">
+              <img src={VitalSignsGrey} />
+            </div>
+            <h2>Aguardando Análise</h2>
+            <p>
+              Faça upload de uma imagem de raio-X e clique em "Iniciar Análise"
+              para obter o diagnóstico
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
