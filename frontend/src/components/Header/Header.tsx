b80b593dfd47dfcbb0cb33474d6b3edd3d0abe5a
andregarcia0412 <andregarcia0412@gmail.com>
2026-03-25 17:06:59 -0300

    :sparkles: feature: header component

---
+++ frontend/src/components/Header/Header.tsx
@@ -0,0 +1,16 @@
+import "./style.header.css";
+import VitalSignsWhite from "../../assets/vital_signs_white.svg";
+
+export const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-icon">
+        <img src={VitalSignsWhite} />
+      </div>
+      <div className="header-title">
+        <h1>ToraxScan AI</h1>
+        <p>Diagnóstico por imagem assistido por IA</p>
+      </div>
+    </div>
+  );
+};
--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -1,44 +1,48 @@
 import { Dropzone } from "../../components/Dropzone/Dropzone";
 import "./style.css";
 import VitalSignsGrey from "../../assets/vital_signs_grey.svg";
+import { Header } from "../../components/Header/Header";
 
 export const Home = () => {
   return (
-    <div className="home-layout">
-      <div className="home-column home-column-left">
-        <Dropzone loading={false} analyzeOnClick={() => {}} />
+    <div>
+      <Header />
+      <div className="home-layout">
+        <div className="home-column home-column-left">
+          <Dropzone loading={false} analyzeOnClick={() => {}} />
 
-        <div className="home-card about-card">
-          <h1>Sobre o sistema</h1>
-          <p>
-            O MediScan AI utiliza algoritmos de inteligência artificial para
-            auxiliar na detecção de condições pulmonares através de imagens de
-            raio-X.
-          </p>
+          <div className="home-card about-card">
+            <h1>Sobre o sistema</h1>
+            <p>
+              O ToraxScan AI utiliza algoritmos de inteligência artificial para
+              auxiliar na detecção de condições pulmonares através de imagens de
+              raio-X.
+            </p>
 
-          <div className="about-card-list">
-            <ul>
-              <li>Suporta imagens JPEG, PNG e outros formatos comuns</li>
-              <li>
-                Detecta COVID-19, pneumonia bacteriana e condições normais
-              </li>
-              <li>Fornece nível de confiança para cada diagnóstico</li>
-            </ul>
+            <div className="about-card-list">
+              <ul>
+                <li>Suporta imagens JPEG, PNG e JPG</li>
+                <li>
+                  Detecta COVID-19, pneumonia bacteriana e condições normais
+                </li>
+                <li>Fornece nível de confiança para cada diagnóstico</li>
+              </ul>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="home-column home-column-right">
-        <div className="home-card home-card-fill">
-          <div className="home-card-waiting">
-            <div className="vital-signs-grey-icon">
-              <img src={VitalSignsGrey} />
+        <div className="home-column home-column-right">
+          <div className="home-card home-card-fill">
+            <div className="home-card-waiting">
+              <div className="vital-signs-grey-icon">
+                <img src={VitalSignsGrey} />
+              </div>
+              <h2>Aguardando Análise</h2>
+              <p>
+                Faça upload de uma imagem de raio-X e clique em "Iniciar
+                Análise" para obter o diagnóstico
+              </p>
             </div>
-            <h2>Aguardando Análise</h2>
-            <p>
-              Faça upload de uma imagem de raio-X e clique em "Iniciar Análise"
-              para obter o diagnóstico
-            </p>
           </div>
         </div>
       </div>
