b61e97fcf0dc50a8aec59c83c1ffb0c9c35eb48b
andregarcia0412 <andregarcia0412@gmail.com>
2026-03-26 17:00:07 -0300

    :bug: fix: replace " " in predictor

--- ai_microservice/src/inference/predictor.py
+++ ai_microservice/src/inference/predictor.py
@@ -7,7 +7,7 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 MODEL_PATH = os.path.join(BASE_DIR, "..", "model", "torax_radiography_disease_classification_model_v2.pth")
 
-class_names = ["Covid", "Lung_Opacity", "Normal", "Viral Pneumonia"]
+class_names = ["Covid", "Lung_Opacity", "Normal", "Viral_Pneumonia"]
 
 model = ToraxRadiographyModel(input_shape=3, hidden_units=32, output_shape=4).to(device)
 model.load_state_dict(
