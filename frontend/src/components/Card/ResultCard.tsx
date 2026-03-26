ff881f384c4641ecee934b11114f0f4e056321a2
andregarcia0412 <andregarcia0412@gmail.com>
2026-03-26 18:01:55 -0300

    :sparkles: feature: finish result card component

--- frontend/src/components/Card/ResultCard.tsx
+++ frontend/src/components/Card/ResultCard.tsx
@@ -1,6 +1,5 @@
 import { ILLNESSES } from "../../data/constants/illnesses";
 import type { ClassificationDto } from "../../data/dto/classification.dto";
-import { classifyProgress } from "../../utils/ClassifyProgress";
 import { classifyProgressNomenclature } from "../../utils/ClassifyProgressNomenclature";
 import "./style.result-card.css";
 import Stethoscope from "../../assets/stethoscope.svg";
@@ -10,6 +9,7 @@ import Warning from "../../assets/warning.svg";
 import Help from "../../assets/help.svg";
 import Check from "../../assets/check.svg";
 import { ProgressBar } from "../ProgressBar/ProgressBar";
+import ErrorBlue from "../../assets/error_blue.svg";
 
 type ResultCardProps = {
   classification: ClassificationDto;
@@ -67,6 +67,34 @@ export const ResultCard = ({ classification }: ResultCardProps) => {
           <div className="result-card-details-body">
             <p>{result.details}</p>
           </div>
+        </div>
+      </div>
+
+      <div className="result-card-recomendations">
+        <div className="result-card-recomendations-title">
+          <img src={Stethoscope} />
+          <h2>Recomendações</h2>
+        </div>
+        <div className="result-card-recomendations-list">
+          <ul>
+            {result.recomendations.map((recomendation: string) => {
+              return <li>{recomendation}</li>;
+            })}
+          </ul>
+        </div>
+      </div>
+
+      <div className="result-card-warning-wrapper">
+        <div className="result-card-warning">
+          <div className="result-card-warning-title">
+            <img src={ErrorBlue} />
+            <p>Aviso Médico</p>
+          </div>
+          <p className="result-card-warning-description">
+            Este diagnóstico foi gerado por inteligência artificial e deve ser
+            usado apenas como ferramenta de apoio. Consulte sempre um médico
+            especialista para confirmação e tratamento adequado.
+          </p>
         </div>
       </div>
     </div>
