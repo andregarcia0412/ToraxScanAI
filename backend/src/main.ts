98bd7a0bba8bfbaec00ee8856d0f614a4300a257
andregarcia0412 <andregarcia0412@gmail.com>
2026-05-18 14:36:35 -0300

    :wrench: chore add validation pipe and install class transformer and class validator

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT ?? 3000;
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
 
   const swaggerConfig = new DocumentBuilder()
     .setTitle('ToraxScan API')
